5a40ca4cf40577143f97d41034d82558ca20654a
jaisweety <jaisweety23@gmail.com>
2023-12-03 21:21:04 +0545

    git commit -m 'profile'

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final List<String> images = [
+    'assets/images/home.png',
+    'assets/images/cocacola.png',
+    'assets/images/sumsung.png',// Add more images as needed
+    // Add more image paths as needed
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("For You"),
+        centerTitle: true,
+        backgroundColor: Colors.blue, // Set your desired background color here
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 0, top: 15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 350,
+              width: 400,
+              child: PageView.builder(
+                itemCount: images.length,
+                itemBuilder: (context, index) {
+                  return Image.asset(images[index]);
+                },
+              ),
+            ),
+            SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Johnsons Baby Cream',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'by Magnum Distributors!',
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, top: 20),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Rs. 700/- Only',
+                  style: TextStyle(
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 0),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'For 1000 Views',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 60, left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.share,
+                        color: Colors.blueAccent,
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        'Share',
+                        style: TextStyle(
+                          fontSize: 19,
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 155,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.black,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "View Campaign",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message, size: 30, color: Colors.grey),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, size: 30, color: Colors.grey),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications, size: 30, color: Colors.grey),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, size: 30, color: Colors.grey),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/influencer_profile_page.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+
+
+class InfluencerProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Influencer Profile"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Profile Image on the page
+            Stack(
+              children: [
+                CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('assets/images/profile.png'),
+                ),
+                Positioned(
+                  bottom: 0,
+                  child: Container(
+                    width: 120,
+                    height: 60,
+                    color: Colors.transparent,
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 5),
+
+            // Center the Container with name, logo, location, and two Text widgets
+            Center(
+              child: Container(
+                width: 350,
+                height: 300,
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Khusbu',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 8),
+
+                    // Row with logo, location, and other details
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                          size: 20,
+                          color: Colors.red,
+                        ),
+                        SizedBox(width: 8),
+                        Text(
+                          'Kathmandu, Nepal ',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black87,
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                      ],
+                    ),
+
+                    SizedBox(height: 8),
+
+                    // Multiline text centered
+                    Center(
+                      child: Text(
+                        'I make motivational, news and facts video and my audience are from 15 to 32 age group. '
+                            'I guarantee 25k views and will make more videos if it dont reach the target views. ',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black87,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+
+                    SizedBox(height: 8),
+
+                    // Small container added to the right of the sentence
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Area of Expertise',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black87,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        SmallContainer(text: 'Motivation'),
+                        SmallContainer(text: 'Lifestyle'),
+                        SmallContainer(text: 'Education'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            
+            // Single row with "Area of Expertise" text and three containers
+          ],
+        ),
+      ),
+
+      // Bottom Navigation Bar
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message, size: 30, color: Colors.grey),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, size: 30, color: Colors.grey),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications, size: 30, color: Colors.grey),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, size: 30, color: Colors.grey),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SmallContainer extends StatelessWidget {
+  final String text;
+
+  const SmallContainer({required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 80,
+      height: 60,
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade200,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 10,
+          color: Colors.black87,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: InfluencerProfile(),
+  ));
+}
